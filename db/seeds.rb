730276eb556e90d059ad091c7535ba444791a649
Tom Brennan <tommybrennan@gmail.com>
2017-06-01 21:06:06 -0400

    seeded the banners data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |banner|
+  Banner.create!(start_date: Date.today, end_date: Date.tomorrow, location: "Jumbotron 2", headline: "#{banner} Don't get shorn!", subcopy: "Sheeplessly sleep the night away", image: "D:/sleeping/images")
+end
+
+
+  puts "10 Banner requests have been created"
