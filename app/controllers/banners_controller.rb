003eb92dd2fd59fb397149e2cb31ce6ea8d4a801
Tom Brennan <tommybrennan@gmail.com>
2017-06-02 19:42:45 -0400

    added the new banners form

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -3,4 +3,20 @@ class BannersController < ApplicationController
   def index
   end
 
+  def new
+    @banner = Banner.new
+  end
+
+  def create
+    @banner = Banner.new(params.require(:banner).permit(:start_date, :end_date, :location, :headline, :subcopy, :image))
+
+    @banner.save
+
+    redirect_to @banner
+  end
+
+  def show
+    @banner = Banner.find(params[:id])
+  end
+
 end
--- spec/features/banner_spec.rb
+++ spec/features/banner_spec.rb
@@ -12,4 +12,26 @@ describe 'navigate' do
       expect(page).to have_content(/Banners/)
     end
   end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_banner_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from a new form page' do
+      visit new_banner_path
+
+      fill_in 'banner[start_date]', with: Date.today
+      fill_in 'banner[end_date]', with: Date.tomorrow
+      fill_in 'banner[location]', with: "Jumbotron 3"
+      fill_in 'banner[headline]', with: "Let the sheep sleep!"
+      fill_in 'banner[subcopy]', with: "Sheep baby sheep!"
+      fill_in 'banner[image]', with: "D:/sleeping/images"
+
+      click_on "Save"
+
+      expect(page).to have_content("Let the sheep sleep!")
+    end
+  end
 end
