6651003c11ab3eef66b6f138374681d3dd0872a5
Tom Brennan <tommybrennan@gmail.com>
2017-06-02 20:26:40 -0400

    implemented user connection with tests

--- spec/features/banner_spec.rb
+++ spec/features/banner_spec.rb
@@ -15,6 +15,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Bill", last_name: "Bixby")
+      login_as(user, :scope => :user)
       visit new_banner_path
     end
 
@@ -23,17 +25,27 @@ describe 'navigate' do
     end
 
     it 'can be created from a new form page' do
-
       fill_in 'banner[start_date]', with: Date.today
       fill_in 'banner[end_date]', with: Date.tomorrow
       fill_in 'banner[location]', with: "Jumbotron 3"
       fill_in 'banner[headline]', with: "Let the sheep sleep!"
       fill_in 'banner[subcopy]', with: "Sheep baby sheep!"
       fill_in 'banner[image]', with: "D:/sleeping/images"
-
       click_on "Save"
 
       expect(page).to have_content("Let the sheep sleep!")
+    end
+
+    it 'will have a user associated with the banner' do
+      fill_in 'banner[start_date]', with: Date.today
+      fill_in 'banner[end_date]', with: Date.tomorrow
+      fill_in 'banner[location]', with: "Jumbotron 3"
+      fill_in 'banner[headline]', with: "Let the sheep sleep!"
+      fill_in 'banner[subcopy]', with: "Sheep baby sheep!"
+      fill_in 'banner[image]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.banners.last.image).to eq("User Association")
     end
   end
 end
