314014a7bc3f30edbef9c72ebeead87129f6e383
Tom Brennan <tommybrennan@gmail.com>
2017-07-04 15:14:56 -0400

    implmented label with status values

--- app/helpers/banners_helper.rb
+++ app/helpers/banners_helper.rb
@@ -1,2 +1,25 @@
 module BannersHelper
+
+  def status_label status
+      status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+    case status
+      when 'submitted'
+        content_tag(:span, status.titleize, class: 'label label-primary')
+      when 'assigned'
+        content_tag(:span, status.titleize, class: 'label label-info')
+      when 'ops-qa'
+        content_tag(:span, status.titleize, class: 'label label-warning')
+      when 'product-qa'
+        content_tag(:span, status.titleize, class: 'label label-primary')
+      when 'completed'
+        content_tag(:span, status.titleize, class: 'label label-success')
+      when 'rejected'
+        content_tag(:span, status.titleize, class: 'label label-danger')
+    end
+  end
 end
