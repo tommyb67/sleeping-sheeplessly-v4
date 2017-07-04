7e941d7b1c3e1737e842edba717188a686fd1d27
Tom Brennan <tommybrennan@gmail.com>
2017-07-04 10:34:44 -0400

    added a custom scope for users

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,4 +2,6 @@ class Banner < ApplicationRecord
   enum status: {submitted: 0, assigned: 1, completed: 2, ops_qa: 3, product_qa: 4, completed: 5, rejected: 6 }
   belongs_to :user, optional: true
   validates_presence_of :start_date, :end_date, :location, :image
+
+  scope :banners_by, ->(user) { where(user_id: user.id) }
 end
